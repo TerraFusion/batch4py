abbeab476c082b1cd4af53ec49de6f6624706aa2
LandonTClipp <clipp2@illinois.edu>
2018-05-16 15:54:10 -0500

    sudo

--- install.sh
+++ install.sh
@@ -5,9 +5,17 @@ sudo python setup.py install
 
 echo "testing batch4py importability..."
 python -c "import batch4py"
+retval=$?
 echo "done"
+if [ $retval -ne 0 ]; then
+    exit $retval
+fi
 
 echo "Performing batch4py test suite..."
-python tests/import.py
+sudo python tests/import.py
+retval=$?
 echo "Done"
 
+if [ $retval -ne 0 ]; then
+    exit $retval
+fi
