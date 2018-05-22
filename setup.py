376864ba410b462a52b1937d1b8bd36c8cfb803a
LandonTClipp <clipp2@illinois.edu>
2018-05-22 10:42:05 -0500

    I'm never gonna get this right

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 import os
 setup(
     name='batch4py',
-    version='0.2.0',
+    version='0.2.2',
     author='Landon T. Clipp',
     author_email='clipp2@illinois.edu',
     packages=['batch4py'],
@@ -13,7 +13,7 @@ setup(
         qsub and allows for users to define complex job chains.',
     install_requires=[ 'pyyaml' ],
     url = 'https://github.com/TerraFusion/batch4py',
-    download_url='https://github.com/TerraFusion/batch4py/archive/0.1.0.tar.gz',
+    download_url='https://github.com/TerraFusion/batch4py/archive/0.2.2.tar.gz',
     keywords = ['batch', 'torque', 'pbs', 'pbs-torque', 'hpc', 'python', 'python3', 'cluster',
         'scheduler', 'schedule' ],
     package_data = {'batch4py': [ os.path.join( '.', 'batch4py', 'config.yml') ] },
